7798d337973f54de14dd4468e8c7ac5a00ea079f
Til Boerner <til@dxdm.org>
2021-03-28 23:26:18 +0200

    Set ZRO and NEG when setting A

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod types {
     pub type Address = Word;
     pub type Data = Byte;
     pub type Flags = Data;
+
+    pub const DATA_WIDTH: u8 = 8;
 }
 
 mod cpu {
@@ -70,6 +72,12 @@ mod cpu {
             let flag_val = if val { self.sr | mask } else { self.sr & !mask };
             self.sr = flag_val;
         }
+
+        pub fn set_a(&mut self, val: Data) {
+            self.a = val;
+            self.set_flag(StatusFlag::ZRO, val == 0);
+            self.set_flag(StatusFlag::NEG, (val >> DATA_WIDTH - 1) > 0);
+        }
     }
 }
 
@@ -77,6 +85,7 @@ mod cpu {
 mod test {
 
     use crate::cpu::*;
+    use crate::types::*;
 
     #[test]
     fn test_get_set_flag() {
@@ -98,6 +107,41 @@ mod test {
         assert_eq!(cpu.get_flag(StatusFlag::ZRO), false);
         cpu.set_flag(StatusFlag::ZRO, false);
         assert_eq!(cpu.get_flag(StatusFlag::NEG), true);
+    }
+
+    #[test]
+    fn test_set_a_sets_a() {
+        let mut cpu = CpuState::new();
+
+        cpu.set_a(42);
+
+        assert_eq!(cpu.a, 42);
+    }
+
+    #[test]
+    fn test_set_a_sets_zro() {
+        let mut cpu = CpuState::new();
+
+        cpu.set_a(0);
+
+        assert_eq!(cpu.get_flag(StatusFlag::ZRO), true);
+
+        cpu.set_a(42);
+
+        assert_eq!(cpu.get_flag(StatusFlag::ZRO), false);
+    }
+
+    #[test]
+    fn test_set_a_sets_neg() {
+        let mut cpu = CpuState::new();
+
+        cpu.set_a(1 << DATA_WIDTH - 1);
+
+        assert_eq!(cpu.get_flag(StatusFlag::NEG), true);
+
+        cpu.set_a(0);
+
+        assert_eq!(cpu.get_flag(StatusFlag::NEG), false);
     }
 }
 
